9c60935b6995c812aa63b4b997caa696854d5169
Ian Moffitt <nessthehero@gmail.com>
2012-05-21 09:35:05 -0400

    Created branch based on modifications for latest project. Take this and parse out what I wanna keep.

--- jquery.easySlider.js
+++ jquery.easySlider.js
@@ -20,6 +20,8 @@ License: http://www.opensource.org/licenses/mit-license.html
         base.$el = $(el);
         base.el = el;	
 		
+		log(base.$el);
+		
 		base.init = function()
         {
 
@@ -27,18 +29,33 @@ License: http://www.opensource.org/licenses/mit-license.html
 			
 			// Stored Values
 			base.length = $(base.options.slide, base.$el).length;	
+
 			base.rotTimeout = 0;
 			base.first = $(base.options.slide, base.$el).eq(0);
 
+			// If there is only one thing, turn off autorotate and paging.
+			if (base.length == 1) {
+				base.options.autorotate = false;
+				base.options.paging = false;
+			}
+
+			// If auto rotate is false, but there are more than one slides, we better turn paging back on.
+			if (base.options.autorotate === false && base.length != 1) {
+				base.options.paging = true;
+			}
+
             // Put your initialization code here
             base.readyShow();
-			base.bindEvents();
-			
+
+			base.options.start(base);	
+
 			if (base.options.autorotate) {
 				base.rotTimeout = setTimeout(function() {
 					base.next();
-				}, base.options.timing);
+				}, (base.options.timing * 2));
 			}
+
+			base.$el.find('.pager').show();
 			
         };
 		
@@ -46,63 +63,114 @@ License: http://www.opensource.org/licenses/mit-license.html
 			
 			// Remove current class if it is assigned
 			$(base.options.slide, base.$el).removeClass(base.options.currentClass);
-			
-			console.log(base.first);
-			
+					
 			// "Preloading" the first slide. They see a loading animation until the image is fully loaded. There 
 			// might be a flash of the image filling in on certain browsers, but they will not see the image while it is barely loaded.
-			if (base.options.preload) {
+			if (base.options.preload && (!$.browser.msie && $.browser.version != "8.0")) {
 				
 				base.$el.append('<div class="loading"><img src="'+base.options.loadimage+'" alt="Loading..." /></div>');
+				log("appended")
 				
 				switch (base.options.preloadMethod) {
 					
-					case 'img':				
-						console.log('/'+base.first.find('img').attr('src'));
-						base.first.find('img').load('/'+base.first.find('img').attr('src'), function() {
+					case 'img':	
+						// console.log(escape(base.first.find('img').attr('src')));
+						// base.first.find('img').load(escape(base.first.find('img').attr('src')), function(text, status, xmlr) {
+						// 	if (status == "success") {
+						// 		base.$el.find('ul').css('opacity','0');
+						// 		base.setupImages();					
+						// 		setTimeout(function() { 
+						// 			base.$el.find('ul').animate({'opacity':'1'}, 1000, 'linear'); 
+						// 			base.$el.find('.loading').remove();
+						// 		}, 1000);
+						// 	}
+						// });
+						base.$el.onImagesLoad( function($selector) {
+
 							base.$el.find('ul').css('opacity','0');
 							base.setupImages();					
 							setTimeout(function() { 
 								base.$el.find('ul').animate({'opacity':'1'}, 1000, 'linear'); 
 								base.$el.find('.loading').remove();
-							}, 1000);
-							
+
+								base.fireTrigger("done");
+							}, 1000);							
+						
 						});
 						break;
 					
 					case 'bg':
 						// I'm so sorry . . . okay it's not that bad
-						var bgSrc = base.first.css('background-image').toString().replace(/url\(/,'').replace(/\)/,'').replace(/https?:\/\/(.+)\.com\//,'').toString();
+						// console.log('it is: ' + base.first.css('backgroundImage').toString());
+						var bgSrc = '/' + base.first.css('backgroundImage').toString().replace(/url\(/,'').replace(/\)/,'').replace(/"/g,'').replace(/https?:\/\/(.+)\.com\//,'').toString();
 						var bgImg = new Image();
-						bgImg.src = bgSrc;
-						base.first.load('/'+bgImg.src, function() {
+						bgImg.onload = function() {
 							base.$el.find('ul').css('opacity','0');
-					//		base.$el.hide();
 							base.setupImages();					
 							setTimeout(function() { 
 								base.$el.find('ul').animate({'opacity':'1'}, 1000, 'linear'); 
 								base.$el.find('.loading').remove();
-							}, 1000);
-					//		setTimeout(function() { base.$el.fadeIn('slow'); }, 1000);
-							
-						});
+
+								base.fireTrigger("done");
+							}, 1000);							
+						}
+						bgImg.src = bgSrc;			
+						// base.first.load(escape(bgImg.src), function() {
+						// 	if (status == "success") {
+						// 		base.$el.find('ul').css('opacity','0');
+						// 		base.setupImages();					
+						// 		setTimeout(function() { 
+						// 			base.$el.find('ul').animate({'opacity':'1'}, 1000, 'linear'); 
+						// 			base.$el.find('.loading').remove();
+						// 		}, 1000);
+						// 	}
+						// });
 						break;
 				}
 					
 			} else {
-				base.setupImages();	
+				base.setupImages();
+				base.fireTrigger("done");
 			}			
-			
 			base.buildPager();
-			
+			base.bindEvents();
+						
 		}
 		
-		base.buildPager = function() {
-			
+		base.buildPager = function() {			
+		//	debugger;
 			if (base.options.paging) {
-				if (base.length > 1) {
-					base.$el.append("<div class='pager'><ul><li class='"+base.options.prevSelector+"'><a class='ir' href='javascript:;'>"+base.options.prevText+"</a></li><li class='"+base.options.nextSelector+"'><a class='ir' href='javascript:;'>"+base.options.nextText+"</a></li></ul></div>");
-				}
+					
+				var pager =	$("<div></div>")
+						.addClass('pager')
+						.css('display','none')
+						.attr('data-slider-id', base.$el.attr('id'))
+						.append(
+							$("<ul></ul>")
+								.append(
+									$("<li></li>")
+										.addClass(base.options.prevSelector)
+										.append(
+											$("<a></a>")
+												.addClass('ir')
+												.attr('href','javascript:;')
+												.html(base.options.prevText)
+										)
+								)
+								.append(
+									$("<li></li>")
+										.addClass(base.options.nextSelector)
+										.append(
+											$("<a></a>")
+												.addClass('ir')
+												.attr('href','javascript:;')
+												.html(base.options.nextText)
+										)
+								)
+						);
+						
+					base.$el.prepend(pager);				
+				
 			}	
 			
 		}
@@ -110,22 +178,49 @@ License: http://www.opensource.org/licenses/mit-license.html
 		base.bindEvents = function() {
 		
 			if (base.options.paging) {
-				$('.'+base.options.prevSelector + ' a', base.$el).on('click', function() {
-					
+
+				$('.'+base.options.prevSelector + ' a', base.$el).on('click', function(e) {
+					e.preventDefault();
+					base.options.before('prev', base);
 					base.prev();
-					
 				});
 				
-				$('.'+base.options.nextSelector + ' a', base.$el).on('click', function() {
-					
+				$('.'+base.options.nextSelector + ' a', base.$el).on('click', function(e) {
+					e.preventDefault();
+					base.options.before('next', base);
 					base.next();
-					
 				});
 			}
 			
 		}
+
+		base.fireTrigger = function(event) {
+			
+			switch (event) {
+				case "done":
+					base.$el.trigger('ez-done', [
+						{
+							id: base.$el.attr('id')
+						}
+					]);
+					break;
+				case "change":
+					base.$el.trigger('ez-change', [
+						{
+							id: base.$el.attr('id')
+						}
+					]);
+					break;
+				default:
+					break;
+			}			
+
+		}
 		
 		base.setupImages = function() {
+
+			log('setup run for ' + base.$el.attr('id'));
+
 			$(base.options.slide, base.$el).each(function(index) {
 				if (base.options.startIndex <= base.length && base.options.startIndex != 0) {
 					if (index != (base.options.startIndex - 1)) {
@@ -147,6 +242,9 @@ License: http://www.opensource.org/licenses/mit-license.html
 					}
 				}
 			});	
+			log('pager - '+base.$el.find('.pager').length);
+			
+			base.$el.find('.pager').show();
 		}
 		
 		base.auto = function() {
@@ -169,6 +267,17 @@ License: http://www.opensource.org/licenses/mit-license.html
 			var nextSlide = $(base.options.slide+':nth-child('+next+')', base.$el);			
 			
 			switch (base.options.effect) {
+				case 'none':
+					currentSlide.hide();
+					nextSlide.show(function () {
+						$(base.options.slide,base.$el).removeClass(base.options.currentClass);
+						$(this).addClass(base.options.currentClass);
+						base.auto();
+						base.options.after('next', base);
+
+						base.fireTrigger('change');
+					});					
+					break;
 				case 'fade':
 				default: 
 					currentSlide.fadeTo(base.options.speed, 0).css('zIndex', 300);
@@ -176,9 +285,12 @@ License: http://www.opensource.org/licenses/mit-license.html
 						$(base.options.slide,base.$el).removeClass(base.options.currentClass);
 						$(this).addClass(base.options.currentClass);
 						base.auto();
+						base.options.after('next', base);
+
+						base.fireTrigger('change');
 					});					
 					break;
-			}				
+			}						
 			
 		}
 		
@@ -193,6 +305,17 @@ License: http://www.opensource.org/licenses/mit-license.html
 			var prevSlide = $(base.options.slide+':nth-child('+prev+')',base.$el);
 			
 			switch (base.options.effect) {
+				case 'none':
+					currentSlide.hide();
+					prevSlide.show(function () {
+						$(base.options.slide,base.$el).removeClass(base.options.currentClass);
+						$(this).addClass(base.options.currentClass);
+						base.auto();
+						base.options.after('next', base);
+
+						base.fireTrigger('change');
+					});					
+					break;
 				case 'fade':
 				default: 
 					currentSlide.fadeTo(base.options.speed, 0).css('zIndex', 300);
@@ -200,13 +323,20 @@ License: http://www.opensource.org/licenses/mit-license.html
 						$(base.options.slide,base.$el).removeClass(base.options.currentClass);
 						$(this).addClass(base.options.currentClass);
 						base.auto();
+						base.options.after('prev', base);
+
+						base.fireTrigger('change');
 					});				
 					break;
-			}
-			
+			}		
+
 		}	
 	
 		base.init();
+
+		base.$el.data('easy-slide', base);
+
+		return base;
 	
 	}
 	
@@ -227,7 +357,10 @@ License: http://www.opensource.org/licenses/mit-license.html
 		preload: false,
 		preloadMethod: 'img',
 		loadimage: '',
-		loadbg: '' /* Not being used */
+		start: function() {},
+		end: function() {},
+		before: function() {},
+		after: function() {}
     };
 
     $.fn.easySlider = function(options)
